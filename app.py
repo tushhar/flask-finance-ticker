7d61c2544cf6c209a299f85e3aed485eda6535b1
tushhar <tushhartalwar@gmail.com>
2015-10-11 19:02:56 +0530

    adding files

---
+++ app.py
@@ -0,0 +1,59 @@
+import json
+import os
+import flask
+from flask import Flask, Response, render_template, send_from_directory, send_file
+
+# Needed for the demo, but perhaps not in actual use
+import itertools
+import random
+import time
+from yahoo_finance import Share
+yahoo = Share('AAPL')
+
+# Server sent events
+class ServerSentEvent:
+    FIELDS = ('event', 'data', 'id')
+    def __init__(self, data, event=None, event_id=None):
+        self.data = data
+        self.event = event 
+        self.id = event_id 
+
+    def encode(self):
+        if not self.data:
+            return ""
+        ret = []
+        for field in self.FIELDS:
+            entry = getattr(self, field) 
+            if entry is not None:
+                ret.extend(["%s: %s" % (field, line) for line in entry.split("\n")])
+        return "\n".join(ret) + "\n\n"
+
+# The Flask application
+app = Flask(__name__)
+
+@app.route('/static/<path:path>')
+def send_js(path):
+    return send_from_directory('static', path)
+
+
+@app.route("/", methods=['GET'])
+def get_index():
+    return render_template('index.html')
+
+
+@app.route("/stream")
+def stream():
+    def gen():
+        count = itertools.count()
+        while True:
+            data = json.dumps({"series": {"mySeries": float(yahoo.get_price())}, 'x': next(count)})
+            ev = ServerSentEvent(data)
+            print(ev.encode())
+            yield ev.encode()
+            time.sleep(0.05)
+
+    return Response(gen(), mimetype="text/event-stream")
+
+
+if __name__ == "__main__":
+    app.run()
